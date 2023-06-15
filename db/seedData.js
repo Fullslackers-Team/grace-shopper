45cb6a7ce89cf820531865dc14c48b5014d00537
Brandon Taylor <bmt1994.tlh@gmail.com>
2023-06-15 11:54:10 -0400

    seed data

--- db/seedData.js
+++ db/seedData.js
@@ -1,1 +1,19 @@
-// Create some seed data and export it from this file
+const users = [
+	{ username: "Brandon", password: 1234 },
+	{ username: "Rian", password: 12345 },
+	{ username: "Maggie", password: 123456 },
+	{ username: "Ali", password: 1234567 },
+	{ username: "Chris", password: 123456789 },
+	{ username: "Pawan", password: 1234567891 },
+	{ username: "Collin", password: 1234567892 },
+];
+
+const products = [];
+
+// const productCategories = [{ name: "food" }, { name: "toys" }];
+
+// const petTypes = [];
+
+const orders = [];
+
+const orderItems = [];
