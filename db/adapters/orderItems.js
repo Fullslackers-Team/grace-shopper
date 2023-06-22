5716bc3aed754170e88ab890739dec7ccb2182aa
rian <rnevcasey@gmail.com>
2023-06-22 10:59:32 -0400

    finished seed, added order items adapter

---
+++ db/adapters/orderItems.js
@@ -0,0 +1,51 @@
+const client = require("../client");
+
+async function addItemToOrder(order_id, product_id) {
+  try {
+    const {
+      rows: [orderItem],
+    } = await client.query(
+      `
+        INSERT INTO order_items(order_id, product_id)
+        VALUES($1,$2)
+        RETURNING *;
+      `,
+      [order_id, product_id]
+    );
+    return orderItem;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function removeItemFromOrder(order_id, product_id) {
+  try {
+    await client.query(
+      `
+        DELETE from order_items
+        WHERE order_id = $1 AND product_id = $2;
+      `,
+      [order_id, product_id]
+    );
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function removeAllItemsFromOrder(order_id) {
+  try {
+    await client.query(
+      `
+        DELETE FROM order_items
+        WHERE order_id = $1
+      `,
+      [order_id]
+    );
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { addItemToOrder, removeItemFromOrder, removeAllItemsFromOrder };
--- db/seed.js
+++ db/seed.js
@@ -1,98 +1,95 @@
-const { createOrders } = require("./adatapters/orders");
-const { createUser } = require("./adatapters/users");
+const { removeAllItemsFromOrder, addItemToOrder } = require("./adapters/orderItems");
+const { createOrders } = require("./adapters/orders");
+const { createProduct } = require("./adapters/products");
+const { createUser } = require("./adapters/users");
 const client = require("./client");
-const { users, orders } = require("./seedData");
+const { users, orders, products, orderItems } = require("./seedData");
 
 async function dropTables() {
-	console.log("Dropping tables...");
-	try {
-		await client.query(
-			`DROP TABLE IF EXISTS users;
+  try {
+    await client.query(
+      `
+	  DROP TABLE IF EXISTS users;
       DROP TABLE IF EXISTS products; 
       DROP TABLE IF EXISTS orders;
-      DROP TABLE IF EXISTS order_items;`
-		);
-	} catch (error) {
-		console.error(error);
-	}
+      DROP TABLE IF EXISTS order_items;
+	  `
+    );
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function createTables() {
-	console.log("Creating tables...");
-	try {
-		await client.query(`CREATE TABLE users(
+  try {
+    await client.query(`CREATE TABLE users(
       id SERIAL PRIMARY KEY,
       username VARCHAR(255) UNIQUE NOT NULL,
       password VARCHAR(255) NOT NULL,
       role VARCHAR(255) NOT NULL DEFAULT 'user'
     );`);
 
-		await client.query(`CREATE TABLE products (
+    await client.query(`CREATE TABLE products (
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) UNIQUE NOT NULL,
-        description VARCHAR(255) UNIQUE NOT NULL,
-        price INTEGER NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        price VARCHAR(255) NOT NULL,
         stock INTEGER NOT NULL
       );`);
 
-		await client.query(`CREATE TABLE orders (
+    await client.query(`CREATE TABLE orders (
         id SERIAL PRIMARY KEY,
         creator_id INTEGER NOT NULL,
         status VARCHAR(255) NOT NULL
       );`);
 
-		await client.query(`CREATE TABLE order_items (
+    await client.query(`CREATE TABLE order_items (
         id SERIAL PRIMARY KEY,
         order_id INTEGER NOT NULL,
         product_id INTEGER NOT NULL
       );`);
-	} catch (error) {
-		console.log(error);
-	}
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function populateTables() {
-	console.log("Populating tables...");
-	try {
-		for (const user of users) {
-			await createUser(user.username, user.password);
-			console.log("users table populated");
-		}
-		// for (const product of products) {
-		// 	await createProduct(product);
-		// 	console.log("products table populated");
-		// }
-		for (const order of orders) {
-			await createOrders(order.creator_id, order.status);
-			console.log("order table populated");
-		}
-		// for (const order_item of order_items) {
-		// 	await createOrder_items(order_item);
-		// 	console.log("order_items table populated");
-		// }
-	} catch (error) {
-		console.error(error);
-	}
+  console.log("Populating tables...");
+  try {
+    for (const user of users) {
+      await createUser(user.username, user.password);
+    }
+    for (const product of products) {
+      await createProduct(product.name, product.price, product.description, product.stock);
+    }
+    for (const order of orders) {
+      await createOrders(order.creator_id, order.status);
+    }
+    for (const order_item of orderItems) {
+      await addItemToOrder(order_item.order_id, order_item.product_id);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function rebuildDb() {
-	client.connect();
-	try {
-		console.log("testing github");
-		console.log("Dropping Tables...");
-		await dropTables();
-		console.log("Tables Dropped");
-		console.log("Creating Tables...");
-		await createTables();
-		console.log("Tables Created");
-		console.log("Populating Tables...");
-		await populateTables();
-		console.log("Tables Populated");
-	} catch (error) {
-		console.error(error);
-	} finally {
-		client.end();
-	}
+  client.connect();
+  try {
+    console.log("Dropping Tables...");
+    await dropTables();
+    console.log("Tables Dropped");
+    console.log("Creating Tables...");
+    await createTables();
+    console.log("Tables Created");
+    console.log("Populating Tables...");
+    await populateTables();
+    console.log("Tables Populated");
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
 }
 
 rebuildDb();
