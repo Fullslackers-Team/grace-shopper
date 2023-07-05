b36983dd24f176c9b96afcd5f13e61126844aec6
Brandon Taylor <bmt1994.tlh@gmail.com>
2023-07-05 10:50:45 -0400

    cart continue to checkout

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -1,25 +1,30 @@
 import "./index.css";
 import {
-	getOrderItems,
 	removeProductFromOrder,
 	removeAllProductsFromOrder,
 } from "../../api/orderItems";
+import { getOrderByCreatorId } from "../../api/orders";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import useAuth from "../../hook/useAuth";
 
 export default function Cart() {
 	const [orderItems, setOrderItems] = useState([]);
+	const { user } = useAuth();
+
 	useEffect(() => {
 		async function fetchOrderItems() {
-			const response = await getOrderItems();
-			setOrderItems(response);
+			const response = await getOrderByCreatorId(user?.id || 0);
+			if (response.data?.length) {
+				setOrderItems(response.data);
+			}
 		}
 		fetchOrderItems();
-	}, []);
+	}, [user]);
 	return (
 		<div className="cart-container">
 			{orderItems
-				? orderItems.map(([orderItem, product_id]) => (
+				? orderItems.map((orderItem, product_id) => (
 						<div key={product_id} className="product-card">
 							<h1 className="product-name">{orderItem.name}</h1>
 							<p className="product-price">{orderItem.price}</p>
@@ -27,18 +32,16 @@ export default function Cart() {
 								<button className="link" onClick={removeProductFromOrder}>
 									Remove item from cart
 								</button>
-								<button className="link" onClick={removeAllProductsFromOrder}>
-									Empty Cart
-								</button>
-							</p>
-							<p className="checkout-button">
-								<Link to="/checkout">
-									<button className="link"> Continue to checkout</button>
-								</Link>
 							</p>
 						</div>
 				  ))
 				: ""}
+
+			<p className="checkout-button">
+				<Link to="/checkout">
+					<button className="link"> Continue to checkout</button>
+				</Link>
+			</p>
 		</div>
 	);
 }
