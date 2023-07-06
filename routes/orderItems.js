655a0862575485d68aee83a74ceabea61a425d36
rian <rnevcasey@gmail.com>
2023-07-06 10:14:29 -0400

    added add products to order route

--- routes/orderItems.js
+++ routes/orderItems.js
@@ -1,46 +1,63 @@
 const express = require("express");
 const router = express.Router();
-const { addItemToOrder, removeItemFromOrder, removeAllItemsFromOrder, getAllItemsFromOrder } = require("../db/adapters/orderItems");
+const {
+	addItemToOrder,
+	removeItemFromOrder,
+	removeAllItemsFromOrder,
+	getAllItemsFromOrder,
+	addItemsToOrder,
+} = require("../db/adapters/orderItems");
 
 router.get("/:orderId", async (req, res, next) => {
-  try {
-    const { orderId } = req.params;
-    const orderItems = await getAllItemsFromOrder(orderId);
-    res.send({ succes: true, name: "Success", message: "Successfully retrieved items in order", data: orderItems });
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const { orderId } = req.params;
+		const orderItems = await getAllItemsFromOrder(orderId);
+		res.send({ succes: true, name: "Success", message: "Successfully retrieved items in order", data: orderItems });
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.post("/:orderId", async (req, res, next) => {
+	try {
+		const { orderId } = req.params;
+		const { products } = req.body;
+		const addedItems = await addItemsToOrder(orderId, products);
+		res.send({ succes: true, name: "Success", message: "Added products to order", data: addedItems });
+	} catch (error) {
+		next(error);
+	}
 });
 
 router.post("/:orderId/:productId", async (req, res, next) => {
-  try {
-    const { orderId, productId } = req.params;
-    const addedItem = await addItemToOrder(orderId, productId);
-    res.send({ succes: true, name: "Success", message: "Added product to order", data: addedItem });
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const { orderId, productId } = req.params;
+		const addedItem = await addItemToOrder(orderId, productId);
+		res.send({ succes: true, name: "Success", message: "Added product to order", data: addedItem });
+	} catch (error) {
+		next(error);
+	}
 });
 
 router.delete("/product/:orderId/:productId", async (req, res, next) => {
-  try {
-    const { orderId, productId } = req.params;
-    await removeItemFromOrder(orderId, productId);
-    res.send({ succes: true, name: "Success", message: "Removed product from order" });
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
+	try {
+		const { orderId, productId } = req.params;
+		await removeItemFromOrder(orderId, productId);
+		res.send({ succes: true, name: "Success", message: "Removed product from order" });
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
 });
 
 router.delete("/all/:orderId", async (req, res, next) => {
-  try {
-    const { orderId } = req.params;
-    await removeAllItemsFromOrder(orderId);
-    res.send({ succes: true, name: "Success", message: "Removed all products from order" });
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const { orderId } = req.params;
+		await removeAllItemsFromOrder(orderId);
+		res.send({ succes: true, name: "Success", message: "Removed all products from order" });
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = router;
