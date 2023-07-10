50ed734ec9d7ee1a5b2a408f61c4e2913cdf4f2a
AliEvanss <schmal.pal17@gmail.com>
2023-07-10 10:17:54 -0500

    changed name in navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,65 +5,65 @@ import { Link } from "react-router-dom";
 import "./index.css";
 import searchBar from "../SearchBar/SearchBar";
 export default function Navbar() {
-	const { setUser, loggedIn, setLoggedIn } = useAuth();
+  const { setUser, loggedIn, setLoggedIn } = useAuth();
 
-	async function handleLogout() {
-		try {
-			await logout();
-			setUser(null);
-			setLoggedIn(false);
-		} catch (err) {
-			console.error(err);
-		}
-	}
-	return (
-		<div className="navbar">
-			<div className="header">
-				<h1>Grace Shopper</h1>
-			</div>
+  async function handleLogout() {
+    try {
+      await logout();
+      setUser(null);
+      setLoggedIn(false);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  return (
+    <div className="navbar">
+      <div className="header">
+        <h1>Paws & Claws</h1>
+      </div>
 
-			<ul className="navlinks">
-				{/* <searchBar /> */}
-				<li>
-					<Link to="/">
-						<button className="link">Home</button>
-					</Link>
-				</li>
-				<li>
-					<Link to="/products">
-						<button className="link">All Products</button>
-					</Link>
-				</li>
-				{loggedIn ? (
-					<li>
-						<Link to="/cart">
-							<button className="link">My Cart</button>
-						</Link>
-					</li>
-				) : (
-					<></>
-				)}
-				{!loggedIn ? (
-					<li>
-						<Link to="/login">
-							<button className="link">Login</button>
-						</Link>
-					</li>
-				) : (
-					""
-				)}
-				{loggedIn ? (
-					<li>
-						<Link to="/">
-							<button className="link" onClick={handleLogout}>
-								Logout
-							</button>
-						</Link>
-					</li>
-				) : (
-					<></>
-				)}
-			</ul>
-		</div>
-	);
+      <ul className="navlinks">
+        {/* <searchBar /> */}
+        <li>
+          <Link to="/">
+            <button className="link">Home</button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/products">
+            <button className="link">All Products</button>
+          </Link>
+        </li>
+        {loggedIn ? (
+          <li>
+            <Link to="/cart">
+              <button className="link">My Cart</button>
+            </Link>
+          </li>
+        ) : (
+          <></>
+        )}
+        {!loggedIn ? (
+          <li>
+            <Link to="/login">
+              <button className="link">Login</button>
+            </Link>
+          </li>
+        ) : (
+          ""
+        )}
+        {loggedIn ? (
+          <li>
+            <Link to="/">
+              <button className="link" onClick={handleLogout}>
+                Logout
+              </button>
+            </Link>
+          </li>
+        ) : (
+          <></>
+        )}
+      </ul>
+    </div>
+  );
 }
