13019b3c7792b1cafbe846272f2577b3fb782245
AliEvanss <schmal.pal17@gmail.com>
2023-07-10 10:43:55 -0500

    CheckoutThanks page name changes

--- client/src/pages/Checkout/CheckoutThanks.jsx
+++ client/src/pages/Checkout/CheckoutThanks.jsx
@@ -3,7 +3,7 @@ import "./checkout.css";
 export default function CheckoutThanks({ setPage }) {
   return (
     <div className="checkout-forms">
-      <h1 className="thank-you">THANK YOU FOR SHOPPING WITH ***!!</h1>
+      <h1 className="thank-you">THANK YOU FOR SHOPPING WITH PAWS & CLAWS!!</h1>
     </div>
   );
 }
