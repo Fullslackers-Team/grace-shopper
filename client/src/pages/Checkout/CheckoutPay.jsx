ca7a41059ca0778c0ed434f7428140717895f61b
AliEvanss <schmal.pal17@gmail.com>
2023-07-10 09:28:59 -0500

    forms made, still need work

--- client/src/pages/Checkout/CheckoutPay.jsx
+++ client/src/pages/Checkout/CheckoutPay.jsx
@@ -1,4 +1,4 @@
-import "./index.css";
+import "./checkout.css";
 import React, { useState } from "react";
 
 export default function CheckoutPay({ setPage }) {
@@ -14,40 +14,35 @@ export default function CheckoutPay({ setPage }) {
         className="checkout-form-pay"
         onSubmit={async (e) => {
           e.preventDefault();
-          await checkoutPay(fullname, cardnumber, expiration, cvv, zipcode);
+          await CheckoutPay(fullname, cardnumber, expiration, cvv, zipcode);
         }}
       >
         <label>Name as it appears on Card:</label>
         <textarea
-          style={{ padding: "5px" }}
           value={fullname}
           onChange={(event) => setFullName(event.target.value)}
         />
 
         <label>Card Number:</label>
         <textarea
-          style={{ padding: "5px" }}
           value={cardnumber}
           onChange={(event) => setCardNumber(event.target.value)}
         />
 
         <label>Expiration:</label>
         <textarea
-          style={{ padding: "5px" }}
           value={expiration}
           onChange={(event) => setExpiration(event.target.value)}
         />
 
         <label>CVV:</label>
         <textarea
-          style={{ padding: "5px" }}
           value={cvv}
           onChange={(event) => setCvv(event.target.value)}
         />
 
         <label>Zip Code:</label>
         <textarea
-          style={{ padding: "5px" }}
           value={zipcode}
           onChange={(event) => setZipCode(event.target.value)}
         />
--- client/src/pages/Checkout/CheckoutShip.jsx
+++ client/src/pages/Checkout/CheckoutShip.jsx
@@ -1,42 +1,57 @@
-import "./index.css";
+import "./checkout.css";
 import React, { useState } from "react";
 
 export default function CheckoutShip({ setPage }) {
-	const [fullname, setFullName] = useState("");
-	const [streetaddress, setStreetAddress] = useState("");
-	const [city, setCity] = useState("");
-	const [state, setState] = useState("");
-	const [zipcode, setZipCode] = useState("");
+  const [fullname, setFullName] = useState("");
+  const [streetaddress, setStreetAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zipcode, setZipCode] = useState("");
 
-	return (
-		<div className="checkout-forms">
-			<form
-				className="checkout-form-ship"
-				onSubmit={async (e) => {
-					e.preventDefault();
-					await checkoutShip(fullname, streetaddress, city, state, zipcode);
-				}}
-			>
-				<label>Full Name:</label>
-				<textarea style={{ padding: "5px" }} value={fullname} onChange={(event) => setFullName(event.target.value)} />
+  return (
+    <div className="checkout-forms">
+      <form
+        className="checkout-form-ship"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          await CheckoutShip(fullname, streetaddress, city, state, zipcode);
+        }}
+      >
+        <label>Full Name:</label>
+        <textarea
+          value={fullname}
+          onChange={(event) => setFullName(event.target.value)}
+        />
 
-				<label>Street Address:</label>
-				<textarea style={{ padding: "5px" }} value={streetaddress} onChange={(event) => setStreetAddress(event.target.value)} />
+        <label>Street Address:</label>
+        <textarea
+          value={streetaddress}
+          onChange={(event) => setStreetAddress(event.target.value)}
+        />
 
-				<label>City:</label>
-				<textarea style={{ padding: "5px" }} value={city} onChange={(event) => setCity(event.target.value)} />
+        <label>City:</label>
+        <textarea
+          value={city}
+          onChange={(event) => setCity(event.target.value)}
+        />
 
-				<label>State:</label>
-				<textarea style={{ padding: "5px" }} value={state} onChange={(event) => setState(event.target.value)} />
+        <label>State:</label>
+        <textarea
+          value={state}
+          onChange={(event) => setState(event.target.value)}
+        />
 
-				<label>Zip Code:</label>
-				<textarea style={{ padding: "5px" }} value={zipcode} onChange={(event) => setZipCode(event.target.value)} />
+        <label>Zip Code:</label>
+        <textarea
+          value={zipcode}
+          onChange={(event) => setZipCode(event.target.value)}
+        />
 
-				<br></br>
-				<button type="submit" onClick={() => setPage("CheckoutPay")}>
-					NEXT
-				</button>
-			</form>
-		</div>
-	);
+        <br></br>
+        <button type="submit" onClick={() => setPage("CheckoutPay")}>
+          NEXT
+        </button>
+      </form>
+    </div>
+  );
 }
