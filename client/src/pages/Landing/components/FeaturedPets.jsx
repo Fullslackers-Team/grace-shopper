fe8ccc534031b533996e34ffd808f538a4c10c04
rian <rnevcasey@gmail.com>
2023-07-12 11:29:19 -0400

    added response information to add to cart buttons and added site icon

--- client/src/pages/Landing/components/FeaturedPets.jsx
+++ client/src/pages/Landing/components/FeaturedPets.jsx
@@ -14,22 +14,26 @@ export default function FeaturedPets() {
 			<div style={{ textAlign: "left" }}>
 				<h1>Featured Pets</h1>
 				<div className="featured-pets-grid" ref={containerRef}>
-					<Link to="/dogs">
+					<Link to="/products">
 						<div className="featured-pet-card dog" onMouseOver={() => setDogHovered(true)} onMouseLeave={() => setDogHovered(false)}>
 							<Slide direction="up" in={dogHovered} mountOnEnter unmountOnExit container={containerRef.current}>
 								<div className="bottom-of-card-text">View Dog Products {"→"}</div>
 							</Slide>
 						</div>
 					</Link>
-					<Link to="/cats">
+					<Link to="/products">
 						<div className="featured-pet-card cat" onMouseOver={() => setCatHovered(true)} onMouseLeave={() => setCatHovered(false)}>
 							<Slide direction="up" in={catHovered} mountOnEnter unmountOnExit container={containerRef.current}>
 								<div className="bottom-of-card-text">View Cat Products {"→"}</div>
 							</Slide>
 						</div>
 					</Link>
-					<Link to="/lizards">
-						<div className="featured-pet-card lizard" onMouseOver={() => setLizardHovered(true)} onMouseLeave={() => setLizardHovered(false)}>
+					<Link to="/products">
+						<div
+							className="featured-pet-card lizard"
+							onMouseOver={() => setLizardHovered(true)}
+							onMouseLeave={() => setLizardHovered(false)}
+						>
 							<Slide direction="up" in={lizardHovered} mountOnEnter unmountOnExit container={containerRef.current}>
 								<div className="bottom-of-card-text">View Lizard Products {"→"}</div>
 							</Slide>
