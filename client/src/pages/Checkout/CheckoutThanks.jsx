0d15830e7ffe3b88a182b7b96558990d74e145b5
AliEvanss <schmal.pal17@gmail.com>
2023-07-12 09:51:23 -0500

    added logo, fixed buttons

--- client/src/pages/Checkout/CheckoutThanks.jsx
+++ client/src/pages/Checkout/CheckoutThanks.jsx
@@ -1,11 +1,14 @@
 import "./checkout.css";
+import logo from "../../assets/LOGO.png";
 
 export default function CheckoutThanks({ setPage }) {
   return (
     <div className="checkout-forms">
       <h1 className="thank-you">
-        THANK YOU FOR <br></br>SHOPPING WITH
+        THANK YOU <br></br>FOR SHOPPING WITH
       </h1>
+
+      <img src={logo} />
     </div>
   );
 }
